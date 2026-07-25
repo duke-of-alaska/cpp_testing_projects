2d2cbc9338c61c6373daba6f276fcb32138496c4
duke-of-alaska <benedictus.marcello@gmail.com>
2026-07-25 22:21:48 +0700

    Implemented dynamic arrays

--- Projects/arrays/arrays.cpp
+++ Projects/arrays/arrays.cpp
@@ -1,14 +1,42 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 using str = std::string;
 
 int main(){
-    str countries[3];
+    std::vector<str> countries;
+    str countryAdd;
+    int choice = 0;
 
-    countries[0] = "Indonesia";
-    countries[1] = "China";
-    countries[2] = "Croatia";
+    do{std::cout << "==== Country Adder ==== " << '\n';
+    std::cout << "1. Add country" << '\n';
+    std::cout << "2. List" << '\n';
+    std::cout << "3. Quit" << '\n';
+    std::cout << "Choice: ";
+    std::cin >> choice;
 
-    std::cout << countries[0] << '\n';
+    switch(choice){
+        case 1:
+            std::cout << "What country would you like to add? ";
+            std::getline(std::cin >> std::ws, countryAdd);
+            countries.push_back(countryAdd);
+            break;
+        case 2:
+            std::cout << '\n' << "==== Country List ==== " << '\n';
+            for (const str& C : countries){
+                std::cout << "- " << C << '\n';
+            }
+            std::cout << "==== ============ ====\n\n";
+        case 3:
+            break;
+        default:
+            std::cout << "Invalid option!";
+            break;
+    }
+    }while (choice != 3);
+
+    
+
     return 0;
 }
