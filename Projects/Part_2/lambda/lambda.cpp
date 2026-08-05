cb6c63e58b3ca6678df6b99b615b759e6b6defe4
duke-of-alaska <benedictus.marcello@gmail.com>
2026-08-05 19:02:25 +0700

    Refined lambda program

--- Projects/Part_2/lambda/lambda.cpp
+++ Projects/Part_2/lambda/lambda.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 
 int main(){
+	int a, b;
+
 	auto multiply = [](int a, int b){
 	return a * b;
 	};
 
-	int result = multiply(4, 5);
+	std::cout << "First number: ";
+	std::cin >> a;
+
+	std::cout << "Second number: ";
+	std::cin >> b;
+
+	int result = multiply(a, b);
 	std::cout << result << '\n';
+	
+	return 0;
 }
-
-
