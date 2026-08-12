c67cf88aa0ba9af526131be65107e48978afdd98
duke-of-alaska <benedictus.marcello@gmail.com>
2026-08-12 19:38:54 +0700

    unordered set

--- Projects/Part_2/unordered_set/unordered_set.cpp
+++ Projects/Part_2/unordered_set/unordered_set.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <print>
 #include <unordered_set>
 
@@ -5,13 +6,48 @@ using std::unordered_set;
 
 int main() {
     unordered_set<int> s;
+    int choice{};
 
-    s.insert(10);
-    s.insert(20);
+    do{
+        std::println("==== Welcome! ====\n"
+                  "1. Add number\n"
+                  "2. List numbers\n"
+                  "3. Quit");
 
-    for (int y : s) {
-        std::print("{}\n", y);
-    }
+        std::println("Select choice:");
+
+        ;
+        if (!(std::cin >> choice)) {
+            std::cerr << "Not a valid choice!";
+            return 1;
+        }
+
+        switch (choice) {
+            case 1: {
+                std::println("What number do you want to add?");
+                int x{};
+                if (!(std::cin >> x)) {
+                    std::cerr << "Not an integer!\n";
+                    return 1;
+                }
+                s.insert(x);
+                break;
+            }
+
+            case 2:
+                for (int y : s) {
+                    std::print("{}\n", y);
+                }
+                break;
+
+            case 3:
+                break;
+
+            default:
+                std::cerr << "Not a valid choice!\n";
+                break;
+        }
+    }while(choice != 3);
 
     return 0;
 }
