f059dd1946805566e9b4d70c3c4b012b4a14a31c
duke-of-alaska <benedictus.marcello@gmail.com>
2026-08-12 20:04:46 +0700

    unordered set final trilogy

--- Projects/Part_2/unordered_set/unordered_set.cpp
+++ Projects/Part_2/unordered_set/unordered_set.cpp
@@ -1,33 +1,41 @@
 #include <iostream>
 #include <print>
 #include <unordered_set>
+#include <algorithm>
+#include <cctype>
+#include <string>
 
 using std::unordered_set;
+using str = std::string;
+str lowercaseAlgo(str& input);
 
 int main() {
     unordered_set<int> s;
     int choice{};
 
     do{
-        std::println("==== Welcome! ====\n"
-                  "1. Add number\n"
-                  "2. List numbers\n"
-                  "3. Quit");
+        std::println("\n==== welcome! ====\n"
+                  "1. add number\n"
+                  "2. list numbers\n"
+                  "3. quit\n"
+                  "4. check existence of number\n"
+                  "5. remove value\n"
+                  "6. nuke everything");
 
-        std::println("Select choice:");
+        std::println("select choice:");
 
         ;
         if (!(std::cin >> choice)) {
-            std::cerr << "Not a valid choice!";
+            std::cerr << "not a valid choice!";
             return 1;
         }
 
         switch (choice) {
             case 1: {
-                std::println("What number do you want to add?");
+                std::println("what number do you want to add?");
                 int x{};
                 if (!(std::cin >> x)) {
-                    std::cerr << "Not an integer!\n";
+                    std::cerr << "not an integer!\n";
                     return 1;
                 }
                 s.insert(x);
@@ -43,11 +51,78 @@ int main() {
             case 3:
                 break;
 
+            case 4: {
+                std::println("what number to find?");
+                int toFind{};
+
+                if (!(std::cin >> toFind)) {
+                    std::cerr << "not a valid number!\n";
+                    return 1;
+                }
+
+                if (s.contains(toFind)) {
+                    std::println("found!");
+                }
+                else {
+                    std::println("not found!");
+                }
+
+                break;
+            }
+
+            case 5: {
+                std::println("what number to remove?");
+                int toRemove{};
+                if (!(std::cin >> toRemove)) {
+                    std::cerr << "not a valid number!\n";
+                    return 1;
+                }
+
+                if (s.erase(toRemove)) {
+                    std::println("removed {}", toRemove);
+                }
+                else {
+                    std::println("{} was not found", toRemove);
+                }
+
+                break;
+            }
+
+            case 6: {
+                std::println("are you sure you want to nuke everything? (y/n)");
+                str confirmation{};
+                std::getline(std::cin >> std::ws, confirmation);
+
+                auto toLowercase = [](str s) {
+                    for (char &c : s) {
+                        c = static_cast<char>(std::tolower(static_cast<unsigned char>(c)));
+                    }
+                    return s;
+                };
+
+                str lowercased = toLowercase(confirmation);
+
+                if (lowercased == "y") {
+                    s.clear();
+                    std::println("entire set nuked");
+                }
+                else if (lowercased == "n") {
+                    std::println("set not nuked");
+                }
+                else {
+                    std::println("invalid choice. return to main menu");
+                }
+
+                break;
+            }
+
+
             default:
-                std::cerr << "Not a valid choice!\n";
+                std::cerr << "not a valid choice!\n";
                 break;
         }
     }while(choice != 3);
 
     return 0;
 }
+
